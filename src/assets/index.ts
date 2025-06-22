1bebb494419c82008e18c4a95f3fc0ab8fa3a883
Tejas Thorat <tejas@netomi.com>
2025-06-22 11:28:18 +0530

    init

---
+++ src/assets/index.ts
@@ -0,0 +1,53 @@
+// Tech Icons
+import css from "./tech/css.png";
+import docker from "./tech/docker.png";
+import figma from "./tech/figma.png";
+import git from "./tech/git.png";
+import html from "./tech/html.png";
+import javascript from "./tech/javascript.png";
+import mongodb from "./tech/mongodb.png";
+import nodejs from "./tech/nodejs.png";
+import reactjs from "./tech/reactjs.png";
+import redux from "./tech/redux.png";
+import tailwind from "./tech/tailwind.png";
+import typescript from "./tech/typescript.png";
+import threejs from "./tech/threejs.svg";
+
+// Company Icons
+import meta from "./company/meta.png";
+import shopify from "./company/shopify.png";
+import starbucks from "./company/starbucks.png";
+import tesla from "./company/tesla.png";
+
+// Service Icons
+import backend from "./service/backend.png";
+import creator from "./service/creator.png";
+import mobile from "./service/mobile.png";
+import web from "./service/web.png";
+
+// Projects
+// Will be using external URLs as defined in constants
+
+export {
+  css,
+  docker,
+  figma,
+  git,
+  html,
+  javascript,
+  mongodb,
+  nodejs,
+  reactjs,
+  redux,
+  tailwind,
+  typescript,
+  threejs,
+  meta,
+  shopify,
+  starbucks,
+  tesla,
+  backend,
+  creator,
+  mobile,
+  web,
+};
